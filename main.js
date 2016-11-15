0fe6544da32d62d4d492304185b37721c7bf1565
parentin <jean-joffrey.parentin@inist.fr>
2016-11-15 11:26:46 +0100

    Ajout. d'un url queryParam pour déclencher le local mode

--- main.js
+++ main.js
@@ -12,36 +12,39 @@
       ;
 
     // Mode de debug local
-    if(window.WEB_HEADER_LOCAL) {
+    if (window.WEB_HEADER_LOCAL
+        || (window.location.href && window.location.href.match(/web_header_local=true/))
+    ) {
       contentDeliveryUrl = window.location.hostname.match(/localhost|127\.0\.0\.1/) && window.location.href || contentDeliveryUrl;
     }
 
     $.ajax({
-      url: contentDeliveryUrl + "public/css/main.min.css",
-      success: function(data) {
+             url    : contentDeliveryUrl + "public/css/main.min.css",
+             success: function(data) {
 
-        $("head").append("<style>" + data + "</style>");
+               $("head").append("<style>" + data + "</style>");
 
-        $.ajax({
-          url: contentDeliveryUrl + "include/surenteteistex.html",
-          success: function(data) {
-            var prependToTarget = window.location.hostname === "article-type.lod.istex.fr" ? ".navbar.navbar-inverse.navbar-fixed-top" : "body";
-            var $webHeader =
-              $(jQuery.parseHTML(data))
-              .filter("#surentete")
-              .find("img").each(rewriteImgUrl).end()
-              .prependTo($(prependToTarget))
-              .wrap("<div id='istex-web-header' class='sandbox'></div>")
-              .find("[href*='#']").click(preventDefaultEvent).end()
-              .find("[href*='" + window.location.hostname + "']").addClass('disabled').click(preventDefaultEvent).end()
-              ;
+               $.ajax({
+                        url    : contentDeliveryUrl + "include/surenteteistex.html",
+                        success: function(data) {
+                          var prependToTarget = window.location.hostname === "article-type.lod.istex.fr" ? ".navbar.navbar-inverse.navbar-fixed-top" : "body";
+                          var $webHeader      =
+                                $(jQuery.parseHTML(data))
+                                  .filter("#surentete")
+                                  .find("img").each(rewriteImgUrl).end()
+                                  .prependTo($(prependToTarget))
+                                  .wrap("<div id='istex-web-header' class='sandbox'></div>")
+                                  .find("[href*='#']").click(preventDefaultEvent).end()
+                                  .find("[href*='" + window.location.hostname + "']").addClass('disabled').click(
+                                  preventDefaultEvent).end()
+                            ;
 
-            window.location.hostname === "www.istex.fr" && $webHeader.find('.logoistex').remove();
-          }
+                          window.location.hostname === "www.istex.fr" && $webHeader.find('.logoistex').remove();
+                        }
 
-        });
-      }
-    });
+                      });
+             }
+           });
 
     function rewriteImgUrl () {
       $(this).attr("src", function(index, attr) {
@@ -53,8 +56,8 @@
 
 
   function loadJqueryAndInvokeInit () {
-    var script = document.createElement("script");
-    script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
+    var script    = document.createElement("script");
+    script.src    = "//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
     script.onload = function() {
       init(window.jQuery.noConflict());
       document.head.removeChild(script);
