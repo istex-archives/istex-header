f3212ef5444806cf210dcdb66eaf114bafb3e725
parentin <jean-joffrey.parentin@inist.fr>
2017-05-19 15:57:28 +0200

    Ajout de l'attribut 'data-header-target' pour choisir ou positionner le bandeau, Reglage du gitignore, Refact. less

--- main.js
+++ main.js
@@ -13,52 +13,58 @@
     loadJqueryAndInvokeInit();
   }
 
+
   function init ($) {
 
     // Mode de debug local
     if ((window.localStorage && window.localStorage.getItem('web_header_local'))
-      || (window.location.search && window.location.search.match(/web_header_local(=true)?(&|$)/))
-      || window.location.hostname.match(/localhost|127\.0\.0\.1/)
-      ) {
+        || (window.location.search && window.location.search.match(/web_header_local(=true)?(&|$)/))
+        || window.location.hostname.match(/localhost|127\.0\.0\.1/)
+    ) {
       ressourcesUrl = window.location.hostname.match(/localhost|127\.0\.0\.1/)
-        && window.location.origin
-        || LOCAL_DELIVERY_URL
-        ;
+                      && window.location.origin
+                      || LOCAL_DELIVERY_URL
+      ;
       console.info('Istex web-header: local mode set on ' + ressourcesUrl);
     }
 
+    var prependToTarget = $('script[data-header-target]').data('headerTarget') || 'body';
+
     $.ajax({
-      url: ressourcesUrl + '/public/css/main.min.css',
-      success: function(data) {
+             url    : ressourcesUrl + '/public/css/main.min.css',
+             success: function(data) {
 
-        $('head').append('<style>' + data + '</style>');
+               $('head').append('<style>' + data + '</style>');
 
-        $.ajax({
-          url: ressourcesUrl + '/public/views/header.view.html',
-          success: function(_data) {
-            var prependToTarget =
-              window.location.hostname === 'article-type.lod.istex.fr' ? '.navbar.navbar-inverse.navbar-fixed-top' : 'body';
+               $.ajax({
+                        url    : ressourcesUrl + '/public/views/header.view.html',
+                        success: function(_data) {
+                          prependToTarget =
+                            window.location.hostname === 'article-type.lod.istex.fr'
+                              ? '.navbar.navbar-inverse.navbar-fixed-top'
+                              : prependToTarget
+                          ;
 
-            var $webHeader =
-              $(jQuery.parseHTML(_data))
-              .filter('#istex-web-header')
-              .find('img').each(rebaseImgUrl).end()
-              .prependTo($(prependToTarget))
-              .wrap('<div class="sandbox"></div>')
-              .find('[href*="#"]').click(preventDefaultEvent).end()
-              .find('[href*="' + window.location.hostname + '"]').addClass('disabled').click(
-              preventDefaultEvent).end()
-              ;
+                          var $webHeader =
+                                $(jQuery.parseHTML(_data))
+                                  .filter('#istex__web-header')
+                                  .find('img').each(rebaseImgUrl).end()
+                                  .prependTo($(prependToTarget))
+                                  .wrap('<div class="sandbox"></div>')
+                                  .find('[href*="#"]').click(preventDefaultEvent).end()
+                                  .find('[href*="' + window.location.hostname + '"]').addClass('disabled').click(
+                                  preventDefaultEvent).end()
+                          ;
 
-            window.location.hostname === 'www.istex.fr' && $webHeader.find('.logoistex').remove();
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.error(textStatus, errorThrown);
-          }
+                          window.location.hostname === 'www.istex.fr' && $webHeader.find('.logoistex').remove();
+                        },
+                        error  : function(jqXHR, textStatus, errorThrown) {
+                          console.error(textStatus, errorThrown);
+                        }
 
-        });
-      }
-    });
+                      });
+             }
+           });
 
     function rebaseImgUrl () {
       $(this).attr('src', function(index, attr) {
@@ -70,8 +76,8 @@
 
 
   function loadJqueryAndInvokeInit () {
-    var script = document.createElement('script');
-    script.src = JQUERY_CDN_URL;
+    var script    = document.createElement('script');
+    script.src    = JQUERY_CDN_URL;
     script.onload = function() {
       init(window.jQuery.noConflict());
       document.head.removeChild(script);
