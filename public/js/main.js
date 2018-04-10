6a30b18754142f86b865857307161eb9b04443f9
Rimet Quentin <34107613+QuentinRimet@users.noreply.github.com>
2018-04-10 14:26:18 +0200

    debut
    le debut du header avec les icones,les logos et une iframe

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+//Permets de charger le header et tous ses fichiers nécessaires.
+function charger(){
+  chargerCSS();
+  chargerNanoAjax();
+}
+
+function chargerCSS(){
+  //Chargement css.
+  var css=document.createElement("link");
+  css.rel="stylesheet";
+  css.href="public/css/main.css";
+  document.head.appendChild(css);
+}
+
+function chargerNanoAjax(){
+  //Chargement nanoAjax.
+  var script = document.createElement("script");
+  script.src = "public/js/nanoajax.min.js";
+  script.onload = function() {
+    //chargement du header après l'installation de nanoAjax.
+    chargerHeader();
+  };
+  document.head.appendChild(script);
+}
+
+//Utilisé après le chargement de nanoAjax.
+function chargerHeader(){
+  nanoajax.ajax({url:"public/views/header.html"}, function (code, responseText) {
+    if(code==200){
+      document.body.innerHTML = responseText+document.body.innerHTML;
+      document.getElementById("header_app").addEventListener("click",function(){
+        affichageApp();
+      });
+    }
+    else
+      alert("Erreur chargement header : error "+code);
+  });
+}
+
+function affichageApp(){
+  var frame= document.getElementById("header_frame");
+  if(frame.style.display=="block")
+    frame.style.display="none";
+  else
+    frame.style.display="block";
+}
+
+//On lance les fonctions.
+charger()
