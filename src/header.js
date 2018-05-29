c171b2fc72d2ab7e274e9bfbf94d0a4c421c5269
rimet <svzsvz@hotmail.fr>
2018-05-29 11:19:53 +0200

    centralisation json

--- src/header.js
+++ src/header.js
@@ -1,11 +1,12 @@
 var script = document.getElementById("iwh_script");
 var ressourceUrl = script.src.split("bundle.js")[0];
-var menuName = script.dataset.menu;
+var url = document.location.href;
 
 import nanoajax from "nanoajax";
 import "./header.css";
 import htmlHeader from "./header.html";
 import readme from "../README.md";
+import services from "./menu.json";
 
 // Permet de charger le header istex
 function loadHeader() {
@@ -21,12 +22,12 @@ function loadHeader() {
     images[i].src = ressourceUrl + "img/" + images[i].dataset.filename + ".svg";
   }
   document.addEventListener("click", function(e) {
-    var services = document.getElementById("iwh_header_block_services");
+    var blockServices = document.getElementById("iwh_header_block_services");
     var icservices = document.getElementById("iwh_header_services");
     if (
       !(
-        e.target == services ||
-        services.contains(e.target) ||
+        e.target == blockServices ||
+        blockServices.contains(e.target) ||
         e.target == icservices ||
         icservices.contains(e.target)
       )
@@ -34,12 +35,12 @@ function loadHeader() {
       document.getElementById("iwh_header_block_services").className = "off";
     }
     var icmenu = document.getElementById("iwh_header_menu");
-    var menu = document.getElementById("iwh_popin_menu");
+    var blockMenu = document.getElementById("iwh_popin_menu");
     if (
       !(
         icmenu == null ||
-        e.target == menu ||
-        menu.contains(e.target) ||
+        e.target == blockMenu ||
+        blockMenu.contains(e.target) ||
         e.target == icmenu ||
         icmenu.contains(e.target)
       )
@@ -62,86 +63,63 @@ function loadHeader() {
       else popin.className = "on";
     });
   loadServices();
-  loadMenu();
   addReadme();
 }
 
 // Permet de charger les services
 function loadServices() {
-  nanoajax.ajax(
-    {
-      url:
-        "https://istex-services.data.istex.fr/api/run/all-documents?maxSize=20",
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    },
-    function(code, responseText) {
-      if (code == 200) {
-        try {
-          var services = JSON.parse(responseText);
-          var html = "<ul id='iwh_popin_services_ul'>";
-          for (var i = 0; i < services.total; i++) {
-            html +=
-              "<li class='iwh_popin_services'><a title=\"" +
-              services.data[i].BNzf +
-              "\" href='" +
-              services.data[i].QoTd +
-              "' class='iwh_services_link'><div class='iwh_services_link_block'><div class='iwh_services_link_block_img'><img src='" +
-              services.data[i].Cl2W +
-              "' alt='" +
-              services.data[i].z351 +
-              "'/></div><p>" +
-              services.data[i].z351 +
-              "</p></div></a></li>";
-          }
-          html += "</ul>";
-          document.getElementById("iwh_popin_services").innerHTML = html;
-        } catch (error) {
-          document.getElementById("iwh_popin_services").innerHTML =
-            "Intégration des services au menu impossible : " + error;
-        }
-      } else
-        document.getElementById("iwh_popin_services").innerHTML = loadError(
-          "iwh_popin_services",
-          code
-        );
+  try {
+    var menu = services.default.menu;
+    var html = "<ul id='iwh_popin_services_ul'>";
+    for (var i = 0; i < services.total; i++) {
+      if (!url.includes(services.data[i].QoTd)) {
+        html +=
+          "<li class='iwh_popin_services'><a title=\"" +
+          services.data[i].BNzf +
+          "\" href='" +
+          services.data[i].QoTd +
+          "' class='iwh_services_link'><div class='iwh_services_link_block'><div class='iwh_services_link_block_img'><img src='" +
+          services.data[i].Cl2W +
+          "' alt='" +
+          services.data[i].z351 +
+          "'/></div><p>" +
+          services.data[i].z351 +
+          "</p></div></a></li>";
+      } else if (services.data[i].menu.length != 0) menu = services.data[i].menu;
     }
-  );
+    html += "</ul>";
+    document.getElementById("iwh_popin_services").innerHTML = html;
+    loadMenu(menu);
+  } catch (error) {
+    document.getElementById("iwh_popin_services").innerHTML =
+      "Intégration des services au menu impossible : " + error;
+  }
 }
 
 // Permet de charger le menu
-function loadMenu() {
-  if (menuName == "" || menuName == undefined) {
+function loadMenu(menu) {
+  if (script.dataset.menu == "hide") {
     var icmenu = document.getElementById("iwh_header_menu");
     icmenu.parentNode.removeChild(icmenu);
-  }
-  try {
-    var menu = require("./menu/" + menuName + ".json");
-    var html = "<ul id='iwh_popin_menu_ul'>";
-    for (var i = 0; i < menu.menu.length; i++) {
-      html +=
-        '<li class="iwh_popin_menu_li"><a class="iwh_popin_menu_a" href="' +
-        menu.menu[i].link +
-        '">';
-
-      if (menu.menu[i].icon != "")
+  } else
+    try {
+      var html = "<ul id='iwh_popin_menu_ul'>";
+      for (var i = 0; i < menu.length; i++) {
         html +=
-          '<img src="' +
-          ressourceUrl +
-          "img/menu/" +
-          menuName +
-          "/" +
-          menu.menu[i].icon +
-          '.svg"/>';
+          '<li class="iwh_popin_menu_li"><a class="iwh_popin_menu_a" href="' +
+          menu[i].link +
+          '">';
 
-      html += menu.menu[i].title + " &#8250</a></li>";
+        if (menu[i].icon != "") html += '<img src="' + menu[i].icon + '"/>';
+
+        html += menu[i].title + " &#8250</a></li>";
+      }
+      html += "</ul>";
+      document.getElementById("iwh_popin_menu").innerHTML = html;
+    } catch (error) {
+      document.getElementById("iwh_popin_menu").innerHTML =
+        "<p >Menu non trouvé : " + error + "</p>";
     }
-    html += "</ul>";
-    document.getElementById("iwh_popin_menu").innerHTML = html;
-  } catch (error) {
-    document.getElementById("iwh_popin_menu").innerHTML =
-      "<p >Menu non trouvé : " + error + "</p>";
-  }
 }
 
 // Permet de charger le statut de la plateforme istex
@@ -180,34 +158,6 @@ function stateIstex() {
           'img/ic_status_unknow.svg" alt="statut"/>';
     }
   );
-}
-
-// Permet de gérer les erreurs de chargement
-function loadError(objet, code) {
-  var debErr = "Le chargement de '" + objet + "' a échoué : ";
-  switch (code) {
-    case 400:
-      return debErr + "contenu de la requête invalide : error 400";
-      break;
-    case 403:
-      return debErr + "ressources interdites d'accès : error 403";
-      break;
-    case 401:
-      return debErr + "accès aux ressources refusé : error 401";
-      break;
-    case 404:
-      return debErr + "ressources non trouvées : error 404";
-      break;
-    case 500:
-      return debErr + "erreur serveur interne : error 500";
-      break;
-    case 503:
-      return debErr + "serveur indisponible : error 503";
-      break;
-    default:
-      return debErr + "error : " + code;
-      break;
-  }
 }
 
 function addReadme() {
